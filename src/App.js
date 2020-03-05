f510856923f2672164214975785ed08afc369c32
Carlos Rotger <carlos@digit.co>
2020-03-05 00:03:38 -0500

    test: peers endpoint

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
+import adapter from 'webrtc-adapter';
 
 function App() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+      const res = await fetch('/api/peers');
+      // const newDate = await res.text();
+      setDate(await res.text());
     }
     getDate();
   }, []);
+
   return (
     <main>
       <h1>Create React App + Go API</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
       </h2>
       <p>
-        <a
-          href="https://github.com/zeit/now/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
+      </p>
+      <p>
         <a href="/api/date">
           <code>api/date</code> for the Date API with Go
         </a>
